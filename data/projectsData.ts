5fba65501a2a6429801238f746472950c31f83bf
MSAnvari <1380msa@gmail.com>
2024-07-12 15:10:45 +0100

    add projects page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,31 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Star Rail Station',
+    description: `A fan-made companion website and database hosting multiple utility apps and tools, created for Honkai Star Rail.
+    Used by thousands of players daily.`,
+    imgSrc: '/static/images/StarRail.png',
+    href: 'https://starrailstation.com/en',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '3D Reconstruction of the Moon in VR',
+    description: `A 3D photorealistic reconstruction of the moon with VR capabilities, created for my final year university project. 
+    Programmed in JavaScript and with WebGL and WebXR.`,
+    imgSrc: '/static/images/Moon.png',
+    href: 'https://github.com/BlueKilvin/VR-3D-Reconstruction',
+  },
+  {
+    title: 'Programmable 16-bit Computer',
+    description: `A program to be installed on my 16-bit computer's EPROM, to allow for simple C++ code executions. 
+    Written for an Arduino with C++.`,
+    imgSrc: '/static/images/16bit-computer.png',
+    href: 'https://github.com/BlueKilvin/16bit-Arduino-Programmer',
+  },
+  {
+    title: 'Gacha Analytics Software',
+    description: `A set of probability distribution analytics and modeling programs, compatible with multiple games.`,
+    imgSrc: '/static/images/Gacha-analytics.png',
+    href: 'https://github.com/BlueKilvin/Gacha-Analytics',
   },
 ]
 
